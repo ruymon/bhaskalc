f53eeb2644c041fc32e28c0052031a0379611a3d
ruymon <ruystfrancis@gmail.com>
2020-08-19 14:59:07 -0300

    Theme Bug Hotfix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,23 +1,23 @@
 function mudarCor() {
 
-    $(".bodyStyle").toggleClass(["bodyStyle", "lightBodyStyle"]);
-    $(".formInput").toggleClass(["formInput", "lightFormInput"]);
-    $(".inputTittle").toggleClass(["inputTittle", "lightInputTittle"]);
-    $(".formula").toggleClass(["formula", "lightFormula"]);
-    $(".inputA").toggleClass(["inputA", "lightInputA"]);
-    $(".inputB").toggleClass(["inputB", "lightInputB"]);
-    $(".inputC").toggleClass(["inputC", "lightInputC"]);
-    $(".inputPH").toggleClass(["inputPH", "lightInputPH"]);
-    $(".botaoLimpar").toggleClass(["botaoLimpar", "lightBotaoLimpar"]);
-    $(".botaoCalcular").toggleClass(["botaoCalcular", "lightBotaoCalcular"]);
-    $(".modoNoturno").toggleClass(["modoNoturno", "lightModoNoturno"]);
-    $(".devCredits").toggleClass(["devCredits", "lightDevCredits"]);
-    $(".graph").toggleClass(["graph", "lightGraph"]);
-    $(".formOutput").toggleClass(["formOutput", "lightFormOutput"]);
-    $(".headerDeltaTratado").toggleClass(["headerDeltaTratado", "lightHeaderDeltaTratado"]);
-    $(".headerX1Tratado").toggleClass(["headerX1Tratado", "lightHeaderX1Tratado"]);
-    $(".headerX2Tratado").toggleClass(["headerX2Tratado", "lightHeaderX2Tratado"]);
-    $(".headerConjuntoSolucaoTratado").toggleClass(["headerConjuntoSolucaoTratado", "lightHeaderConjuntoSolucaoTratado"]);
+    $("#bodyStyle").toggleClass(["bodyStyle", "lightBodyStyle"]);
+    $("#formInput").toggleClass(["formInput", "lightFormInput"]);
+    $("#inputTittle").toggleClass(["inputTittle", "lightInputTittle"]);
+    $("#formula").toggleClass(["formula", "lightFormula"]);
+    $("#coeficienteA").toggleClass(["inputA", "lightInputA"]);
+    $("#coeficienteB").toggleClass(["inputB", "lightInputB"]);
+    $("#coeficienteC").toggleClass(["inputC", "lightInputC"]);
+    $("#inputPH").toggleClass(["inputPH", "lightInputPH"]);
+    $("#botaoLimpar").toggleClass(["botaoLimpar", "lightBotaoLimpar"]);
+    $("#botaoCalcular").toggleClass(["botaoCalcular", "lightBotaoCalcular"]);
+    $("#modoNoturno").toggleClass(["modoNoturno", "lightModoNoturno"]);
+    $("#devCredits").toggleClass(["devCredits", "lightDevCredits"]);
+    $("#graph").toggleClass(["graph", "lightGraph"]);
+    $("#formOutput").toggleClass(["formOutput", "lightFormOutput"]);
+    $("#headerDeltaTratado").toggleClass(["headerDeltaTratado", "lightHeaderDeltaTratado"]);
+    $("#headerX1Tratado").toggleClass(["headerX1Tratado", "lightHeaderX1Tratado"]);
+    $("#headerX2Tratado").toggleClass(["headerX2Tratado", "lightHeaderX2Tratado"]);
+    $("#headerConjuntoSolucaoTratado").toggleClass(["headerConjuntoSolucaoTratado", "lightHeaderConjuntoSolucaoTratado"]);
 };
 
 $("#mudarCor").click(mudarCor);
