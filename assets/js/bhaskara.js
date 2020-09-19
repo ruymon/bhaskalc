2917226ad422616bb9f44bd8ae983d472506a083
ruymon <ruystfrancis@gmail.com>
2020-09-19 14:46:42 -0300

    Patch Fix - Show Graph with DELTA

--- assets/js/bhaskara.js
+++ assets/js/bhaskara.js
@@ -4,13 +4,19 @@
 
 function calcularDelta(a, b, c) {
     const valorDelta = (b ** 2) - 4 * (a * c);
+
+    if (valorDelta < 0) {
+        throw new Error('Não há Raizes no conjunto de números Reais');
+
+    };
+
     return valorDelta;
 };
 
 function calcularBhaskara(a, b, c) {
     const delta = calcularDelta(a, b, c);
-    const raizDelta = Math.sqrt(Math.abs(delta)); // V1.1 - Add Math.abs para tornar delta um valor POSITIVO para calcular a Raiz
-    const x1 = (-b + raizDelta) / (2 * a); // V1.1 - Bug Fix 
+    const raizDelta = Math.sqrt(Math.abs(delta));
+    const x1 = (-b + raizDelta) / (2 * a);
     const x2 = (-b - raizDelta) / (2 * a);
 
     return {
@@ -22,10 +28,13 @@ function calcularBhaskara(a, b, c) {
 
 function calcularResultado() {
 
+    console.log('FIX.JS');
+
     const A = parseFloat($('#coeficienteA').val());
     const B = parseFloat($('#coeficienteB').val());
     const C = parseFloat($('#coeficienteC').val());
 
+    // Condições & Erros
 
     if (isNaN(A) || isNaN(B) || isNaN(C)) {
         mostrarModalAlerta('error', 'Opps...', 'Faltando parametros.');
@@ -40,42 +49,44 @@ function calcularResultado() {
     document.getElementById('graphText').style.display = 'none';
     document.getElementById('plotGraph').style.display = 'block';
 
+    functionPlot({
+        target: document.querySelector("#plotGraph"),
+        width: 700,
+        height: 600,
+        yAxis: {
+            domain: [-1, 9]
+        },
+        grid: true,
+        data: [{
+            // fn: 'x^2 + 2x + 3', // f(x) = ax² + bx + c
+            fn: `${A}x^2 + ${B}x + ${C}`
+        }]
+    });
+
+
+
     try {
         const { x1, x2 } = calcularBhaskara(A, B, C);
 
-        let resultadoDelta = calcularDelta(A, B, C);
+        document.getElementById("resultadoDeltaTratado").innerHTML = calcularDelta(A, B, C).toFixed(2);
+        document.getElementById("x1Tratado").innerHTML = x1.toFixed(2);
+        document.getElementById("x2Tratado").innerHTML = x2.toFixed(2);
 
-        if (resultadoDelta >= 0) {
-            document.getElementById("resultadoDeltaTratado").innerHTML = resultadoDelta.toFixed(2);
-            document.getElementById("x1Tratado").innerHTML = x1.toFixed(2);
-            document.getElementById("x2Tratado").innerHTML = x2.toFixed(2);
+        document.getElementById("resCS1Tratado").innerHTML = x1.toFixed(2);
+        document.getElementById("resCS2Tratado").innerHTML = x2.toFixed(2);
 
-            document.getElementById("resCS1Tratado").innerHTML = x1.toFixed(2);
-            document.getElementById("resCS2Tratado").innerHTML = x2.toFixed(2);
 
-            functionPlot({
-                target: document.querySelector("#plotGraph"),
-                width: 695,
-                height: 690,
-                yAxis: {
-                    domain: [-1, 9]
-                },
-                grid: true,
-                data: [{
-                    // fn: 'x^2 + 2x + 3', // f(x) = ax² + bx + c
-                    fn: `${A}x^2 + ${B}x + ${C}`
-                }]
-            });
 
-        } else {
-            mostrarModalAlerta('error', 'Opps...', 'Não há Raizes no conjunto de números Reais');
-            setTimeout(() => {
-                resetarValores();
-            }, 2501)
-        };
-    } catch {
-        mostrarModalAlerta('error', 'Opps...', 'Algo deu errado... ');
-    };
+    } catch (e) {
+        mostrarModalAlerta('info', 'Aviso', e.message);
+
+        document.getElementById("x1Tratado").innerHTML = 'TESTE';
+        document.getElementById("x2Tratado").innerHTML = 'TESTE';
+
+        document.getElementById("resCS1Tratado").innerHTML = 'TESTE';
+        document.getElementById("resCS2Tratado").innerHTML = 'TESTE';
+
+    }
 };
 
 
@@ -88,12 +99,12 @@ function resetarValores() {
     document.getElementById('graphText').style.display = 'block';
     document.getElementById('plotGraph').style.display = 'none';
 
-    document.getElementById("resultadoDeltaTratado").innerHTML = "value";
-    document.getElementById("x1Tratado").innerHTML = "value";
-    document.getElementById("x2Tratado").innerHTML = "value";
+    document.getElementById("resultadoDeltaTratado").innerHTML = 'value';
+    document.getElementById("x1Tratado").innerHTML = 'value';
+    document.getElementById("x2Tratado").innerHTML = 'value';
 
-    document.getElementById("resCS1Tratado").innerHTML = "";
-    document.getElementById("resCS2Tratado").innerHTML = "";
+    document.getElementById("resCS1Tratado").innerHTML = '';
+    document.getElementById("resCS2Tratado").innerHTML = '';
 
 };
 
